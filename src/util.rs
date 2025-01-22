f180549c4af0e1ab99ac7c0a6665dc2a24c970f5
Huskyto Gonar <huskygonar@gmail.com>
2025-01-22 11:56:41 -0500

    Update utils

--- src/util.rs
+++ src/util.rs
@@ -1,10 +1,12 @@
 
 use std::path::Path;
 
+use rand::Rng;
 use rocket::tokio::fs;
+use rand::distributions::Alphanumeric;
 
-use crate::model::FileData;
 use crate::config::Config;
+use crate::model::FileDefinition;
 
 
 pub struct Util;
@@ -21,20 +23,23 @@ impl Util {
         let path = Path::new(full_path);
         path.exists()
     }
-    pub fn full_path(file_data: &FileData) -> String {
+    pub fn full_path(file_def: &FileDefinition) -> String {
         let path = Path::new(&Config::get_base_path())
-                    .join(&file_data.path)
-                    .join(&file_data.name);
+                    .join(&file_def.id);
+                    // .join(&file_def.name);
+        // let path = Path::new(&Config::get_base_path())
+        //             .join(&file_def.path)
+        //             .join(&file_def.name);
 
         path.to_str().expect("Invalid path").to_string()
     }
-    pub fn full_path_with(path: &str, name: &str) -> String {
-        let path = Path::new(&Config::get_base_path())
-                    .join(path)
-                    .join(name);
+    // pub fn full_path_with(path: &str, name: &str) -> String {
+    //     let path = Path::new(&Config::get_base_path())
+    //                 .join(path)
+    //                 .join(name);
 
-        path.to_str().expect("Invalid path").to_string()
-    }
+    //     path.to_str().expect("Invalid path").to_string()
+    // }
     pub fn split_full_path(full_path: &str) -> (String, String) {
         let _path = Path::new(full_path);
         let path = _path.parent().expect("Invalid path")
@@ -45,5 +50,17 @@ impl Util {
                 .to_string();
 
         (path, name)
+    }
+
+    pub fn checksum(content: &Vec<u8>) -> String {
+        let digest = md5::compute(content);
+        format!("{:x}", digest)
+    }
+    pub fn new_id() -> String {
+        rand::thread_rng()
+            .sample_iter(&Alphanumeric)
+            .take(16)
+            .map(char::from)
+            .collect()
     }
 }
